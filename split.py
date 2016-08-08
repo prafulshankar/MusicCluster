8946daefaf3789124fd8c187607a9737cfbbb2ec
PrafulShankar <prflshankar@gmail.com>
2016-08-08 10:07:46 +0530

    working slicing script from mp3 to 5 mp3 files

--- split.py
+++ split.py
@@ -4,22 +4,25 @@ import pydub
 import os
 
 directory_name = sys.argv[1]
-
-files_to_convert = os.listdir(directory_name)
+print directory_name
+files_to_conv = os.listdir(directory_name)
+files_to_convert = [each for each in files_to_conv if each[-3:]=="mp3"]
+print files_to_convert
 
 def export_mp3_parts(filename, audio):
 	def song_split(song):
 		length = len(song)
 		splits = [0.2*length, 0.4*length, 0.6*length, 0.8*length, length]
 		splits = [song[:0.2*length], song[0.2*length:0.4*length], song[0.4*length:0.6*length], song[0.6*length:0.8*length], song[0.8*length:]]
-		return [part[(len(sound)//2)-5000:(len(part)//2)+5000] for part in splits]
+		return [part[(len(part)//2)-5000:(len(part)//2)+5000] for part in splits]
 	
-	split_sound = sound_split(audio)
-	
+	split_sound = song_split(audio)
+		
 	for part in range(len(split_sound)):
-		split_sound[part].export(filename[0:-4] + str(part) + ".mp3")
+		split_sound[part].export(filename[0:-4] + "_" + str(part+1) + ".mp3")
 
 for fil in files_to_convert:
+	fil = directory_name +  "/" + fil
 	print fil
  	mp3_audio = pydub.AudioSegment.from_file(fil, "mp3")
 	export_mp3_parts(fil, mp3_audio)
